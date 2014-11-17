ef9f0cf7ff9ca2e078ba255f5dcda38431319360
jatr <jaroslaw.trykowski@mobica.com>
2014-11-17 13:05:29 +0100

    creating example classes

--- Table.py
+++ Table.py
@@ -1,25 +1,36 @@
 
 #-*- coding: UTF-8 -*-
 import re
+from write_to_file import Write
 
-fil = open('foo.xml', 'r')
 
-for f in fil:
-  m = re.search(r'^<a.+(EUR|USD|CHF|GBP)\W+a>$', f, re.M)
-  if m: 
-    print f
-    for w in fil:
-      mn = re.search(r'(<td.+currency_table_)([a-z]+)', w, re.M)
-      if mn:
-        s = mn.group(2)
-        print w
-        if s == "avg":
-          break
+class Kantor:
+  
+  def extract(self):
+    fil = open('foo.xml', 'r')
+    for f in fil:
+      m = re.search(r'^<a.+(EUR|USD|CHF|GBP)\W+a>$', f, re.M)
+      if m: 
+        print f
+        for w in fil:
+          mn = re.search(r'(<td.+currency_table_)([a-z]+)', w, re.M)
+          if mn:
+            s = mn.group(2)
+            print w
+            if s == "avg":
+              break
+    fil.close()
+
       
+address = ["https://internetowykantor.pl/kursy-walut/","http://cinkciarz.pl/kantor/kursy-walut-cinkciarz-pl", 
+           "https://liderwalut.pl/kursy-walut" ]
+to_file = Write(address)
+to_file.to_file()
+      
+kantor = Kantor()
+kantor.extract()
 
-fil.close()
 
-print "ok"
 
 
 
--- write_to_file.py
+++ write_to_file.py
@@ -1,22 +1,23 @@
 import mechanize
  
-address = ["https://internetowykantor.pl/kursy-walut/","http://cinkciarz.pl/kantor/kursy-walut-cinkciarz-pl", 
-           "https://liderwalut.pl/kursy-walut" ]
 
-def to_file (name):
-  br=mechanize.Browser()
-  fo = open("foo.xml", "w")
-  for i in name:
-    br.open(i)
-    the_page = br.response().readlines()
-    try:
-      fo.write("\n\n\n<!--******************** "+str(i)+" website content ********************-->\n\n\n")
-      for j in the_page:
-        fo.write(j.lstrip())
-    except IOError,e:
-      print "Error: can\'t write a file: ",e
-    else:
-      print"Content of website: \"%s\" written to a file succesfully!" % i  
-  fo.close()
+class Write:
+  def __init__(self, address):
+      self.address = address
+      
+  def to_file (self):
+    br=mechanize.Browser()
+    fo = open("foo.xml", "w")
+    for i in self.address:
+      br.open(i)
+      the_page = br.response().readlines()
+      try:
+        fo.write("\n\n\n<!--******************** "+str(i)+" website content ********************-->\n\n\n")
+        for j in the_page:
+          fo.write(j.lstrip())
+      except IOError,e:
+        print "Error: can\'t write to a file: ",e
+      else:
+        print"Content of website: \"%s\" written to a file succesfully!" % i  
+    fo.close()
   
-to_file(address)
