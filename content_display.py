c25feb5e27726d327320577964688ada8834399e
trykowski1 <jaroslaw.trykowski@mobica.com>
2014-11-20 23:28:07 +0100

    code refactoring

--- content_display.py
+++ content_display.py
@@ -2,5 +2,5 @@ from content_extract import Input_output_data, Create_timer
 
 input_output = Input_output_data()
 input_output.input_data()
-timer = Create_timer(4,input_output.output_data)
+timer = Create_timer(8,input_output)
 timer.start()
--- content_extract.py
+++ content_extract.py
@@ -6,11 +6,7 @@ import time
 
 class Extract():
   def __init__(self,regxp ):
-    #currency_list: EUR, USD, CHF, GBP; values_list: [(Buy, 4,5667),...]
-    #self.currency_list = []
-    #self.values_list = []
     self.exchange = {}
-    #"currency_reg" - regexp to extract currency, "value_reg" - regexp to extract value
     self.regxp = regxp
   def extract(self):
     status = ["buy","sell"]
@@ -20,24 +16,18 @@ class Extract():
       m = re.search(r"%s" % self.regxp['currency'], f, re.M) 
       if m: 
         i = 0
-        #print m.group(2)
         self.exchange[m.group(2)] = {}
-        #self.currency_list.append(m.group(2))
         for w in fil:
           #extract currency values
           mn = re.search(r"%s" % self.regxp['value'], w, re.M)
           if mn:
             i += 1
-            #print mn.group(2),mn.group(4)
             self.exchange[m.group(2)][status[i-1]] = mn.group(4)
-            #self.values_list.append((status[i-1],mn.group(4)))
             if i == 2:
               break
     fil.close()
 
 class Input_output_data():
-  def __init__(self):
-    self.kantor_data = {}
     
   def input_data(self):
     address = ["https://internetowykantor.pl/kursy-walut/","http://cinkciarz.pl/kantor/kursy-walut-cinkciarz-pl", 
@@ -57,6 +47,8 @@ class Input_output_data():
     self.cinkciarz.extract()
     self.lider.extract()
     self.kantor_data = self.kantor.exchange
+    self.cinkciarz_data = self.cinkciarz.exchange
+    self.lider_data = self.lider.exchange
 
 class Create_timer():
   def __init__(self,time, time_handler):
@@ -64,16 +56,27 @@ class Create_timer():
     self.time_handler = time_handler
   def start(self):
     while True:
-      self.time_handler() 
+      self.time_handler.output_data() 
+      printing = Print(self.time_handler.kantor_data, self.time_handler.cinkciarz_data, self.time_handler.lider_data )
+      printing.printing()
       time.sleep(self.time)
+      
+class Print():
+    def __init__(self, *to_print):
+        self.to_print = to_print
+        
+    def printing(self):
+        for i in self.to_print:
+         print i
 
-    
+'''    
 input_output = Input_output_data()
 input_output.input_data()
-timer = Create_timer(4,input_output.output_data)
+timer = Create_timer(8,input_output)
 timer.start()
+'''
 
-print input_output.kantor_data
+
 
 
 
